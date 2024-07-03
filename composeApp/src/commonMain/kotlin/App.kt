ba336a20676d3d11baceb84aa01e3a1f41a4a181
yassin <yassin.el.megdar.09.35@gmail.com>
2024-07-03 11:31:44 +0200

    changed button color

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,29 +1,17 @@
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import util.Gelds
 import util.toHumanReadableString
@@ -32,7 +20,16 @@ import vw.GameViewModel
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    MaterialTheme(
+        colors = darkColors(
+            primary = Color(0xFF00FF00),
+            background = Color(0xFF000000),
+            surface = Color(0xFF121212),
+            onPrimary = Color.Black,
+            onBackground = Color.White,
+            onSurface = Color.White
+        )
+    ) {
         Screen()
     }
 }
@@ -41,6 +38,7 @@ fun App() {
 @Preview
 fun Screen() {
     Scaffold(
+        backgroundColor = MaterialTheme.colors.background,
         content = {
             val coroutineScope = rememberCoroutineScope()
             val viewModel by remember {
@@ -62,31 +60,59 @@ fun Screen() {
             }
 
             Column(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .verticalScroll(rememberScrollState())
+                    .padding(16.dp)
             ) {
                 Text(
-                    "Idle Game",
-                    style = MaterialTheme.typography.h1,
+                    "PandaKrieger Tycoon",
+                    style = MaterialTheme.typography.h4.copy(
+                        color = MaterialTheme.colors.primary,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp
+                    ),
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
+
                 Spacer(modifier = Modifier.height(16.dp))
 
                 Button(
-                    onClick = { viewModel.reset() }
+                    onClick = { viewModel.reset() },
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        backgroundColor = Color(0xFF00FF00)
+                    ),
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
                 ) {
                     Text("Reset Game")
                 }
 
+                Spacer(modifier = Modifier.height(16.dp))
+
                 gameState?.let { state ->
                     Text(
                         "Bank: ${currentMoney?.toHumanReadableString()} Gelds",
-                        style = MaterialTheme.typography.h4,
+                        style = MaterialTheme.typography.h5.copy(
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
                     )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
                     Button(
-                        onClick = { viewModel.clickMoney(state) }
+                        onClick = { viewModel.clickMoney(state) },
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = Color.Black,
+                            backgroundColor = Color(0xFF00FF00)
+                        ),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
                     ) {
-                        Text("Click money")
+                        Text("Click Money")
                     }
+
+                    Spacer(modifier = Modifier.height(16.dp))
 
                     state.availableJobs.forEach { availableJob ->
                         Generator(
@@ -95,6 +121,7 @@ fun Screen() {
                             onBuy = { viewModel.addWorker(state, availableJob) },
                             onUpgrade = { viewModel.upgradeJob(state, availableJob) }
                         )
+                        Spacer(modifier = Modifier.height(16.dp))
                     }
                 }
             }
@@ -112,26 +139,78 @@ private fun Generator(
 ) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .padding(8.dp)
-            .background(Color.LightGray, RoundedCornerShape(8.dp))
-            .padding(8.dp)
+            .background(
+                Brush.linearGradient(
+                    colors = listOf(Color(0xFF1A1A1A), Color(0xFF2C2C2C)),
+
+                ),
+                RoundedCornerShape(8.dp)
+            )
+            .padding(16.dp)
     ) {
         Column {
-            Text("Generator ${gameJob.id}")
-            Text("Level: ${gameJob.level.level}")
-            Text("Costs: ${gameJob.level.cost.toHumanReadableString()} Gelds")
-            Text("Earns: ${gameJob.level.earn.toHumanReadableString()} Gelds")
-            Text("Duration: ${gameJob.level.duration.inWholeSeconds} Seconds")
+            Text(
+                "Generator ${gameJob.id}",
+                style = MaterialTheme.typography.subtitle1.copy(
+                    color = Color(0xFF00FF00),
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Text(
+                "Level: ${gameJob.level.level}",
+                style = MaterialTheme.typography.body2.copy(
+                    color = Color.White
+                )
+            )
+            Text(
+                "Costs: ${gameJob.level.cost.toHumanReadableString()} Gelds",
+                style = MaterialTheme.typography.body2.copy(
+                    color = Color.White
+                )
+            )
+            Text(
+                "Earns: ${gameJob.level.earn.toHumanReadableString()} Gelds",
+                style = MaterialTheme.typography.body2.copy(
+                    color = Color.White
+                )
+            )
+            Text(
+                "Duration: ${gameJob.level.duration.inWholeSeconds} Seconds",
+                style = MaterialTheme.typography.body2.copy(
+                    color = Color.White
+                )
+            )
         }
+        Spacer(modifier = Modifier.weight(1f))
         if (!alreadyBought) {
-            Button(onClick = onBuy) {
+            Button(
+                onClick = onBuy,
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.Black,
+                    backgroundColor = Color(0xFF00FF00)
+                )
+            ) {
                 Text("Buy")
             }
         } else {
-            Text("Bought")
+            Text(
+                "Bought",
+                style = MaterialTheme.typography.subtitle1.copy(
+                    color = Color(0xFF00FF00),
+                    fontWeight = FontWeight.Bold
+                )
+            )
         }
-        Button(onClick = onUpgrade) {
+        Button(
+            onClick = onUpgrade,
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.Black,
+                backgroundColor = Color(0xFF00FF00)
+            )
+        ) {
             Text("Upgrade")
         }
     }
